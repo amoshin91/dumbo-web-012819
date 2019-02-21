791040efd0520715d4f60671dc719aea6ace2837
Graham Troyer-Joy <graham.troyer-joy@flatironschool.com>
2019-02-21 13:58:07 -0500

    full crud

--- 11-rest/dogs/app/controllers/application_controller.rb
+++ 11-rest/dogs/app/controllers/application_controller.rb
@@ -8,10 +8,12 @@ class ApplicationController < Sinatra::Base
     set :method_override, true
   end
 
+  # welcome
   get "/" do
     erb :welcome
   end
 
+  # index
   get "/dogs" do
     @dogs = Dog.all
     erb :index
@@ -22,16 +24,46 @@ class ApplicationController < Sinatra::Base
   #   erb :show
   # end
 
+  # New
+  get "/dogs/new" do
+    @dog = Dog.new
+    erb :new
+  end
+
+  # create
+  post '/dogs' do
+    # create a new instance of the dog
+    # use the params to set stuff on the dog
+    @dog = Dog.new(params[:dog])
+    # save the dog
+    @dog.save
+    # redirect to the dog show page for the one that was just created
+    redirect to "/dogs/#{ @dog.id }"
+  end
+
+  # Show
   get '/dogs/:id' do
     get_dog
     erb :show
   end
 
+  # edit
   get "/dogs/:id/edit" do
     get_dog
     erb :edit
   end
 
+  # update
+  patch '/dogs/:id' do
+    # binding.pry
+    get_dog
+    # params.delete(:"_method")
+    @dog.update(params[:dog])
+
+    redirect "/dogs/#{ @dog.id }"
+  end
+
+  #delete 
   delete "/dogs/:id" do
     # p "HI FROM THE DELETE 🐕 action"
     get_dog
@@ -39,9 +71,17 @@ class ApplicationController < Sinatra::Base
     redirect to "/dogs"
   end
 
-  patch '/dogs/:id' do
-    "Hello World"
-  end
+
+  # params = {
+  #   _method: "PATCH",
+  #   id: 3,
+  #   dog: {
+  #     name: "Gizmo XIV",
+  #     tail_length: -2
+  #   }
+  # }
+
+
 
   def get_dog
     @dog = Dog.find(params[:id])
