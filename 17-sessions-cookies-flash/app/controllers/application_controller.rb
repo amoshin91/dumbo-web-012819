74d1b5867bed25bf4ac1665def49e849ada557ee
Graham Troyer-Joy <graham.troyer-joy@flatironschool.com>
2019-03-04 12:21:12 -0500

    sessions cookes flash

--- 17-sessions-cookies-flash/app/controllers/application_controller.rb
+++ 17-sessions-cookies-flash/app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 class ApplicationController < ActionController::Base
 
+  before_action :setup_voting
+
+  def setup_voting
+    session["votes"] ||= 5
+    @votes = session["votes"]
+    @can_vote = @votes > 0
+    @errors = flash[:errors]
+  end
+
+  def use_vote
+    @votes -= 1
+    session["votes"] = @votes
+  end
 
 end
--- 17-sessions-cookies-flash/app/controllers/votes_controller.rb
+++ 17-sessions-cookies-flash/app/controllers/votes_controller.rb
@@ -1,7 +1,12 @@
 class VotesController < ApplicationController
 
   def create
-    @vote = Vote.create(color_id: params[:color_id])
+    if @can_vote
+      @vote = Vote.create(color_id: params[:color_id])
+      use_vote
+    else
+      flash[:errors] = [ "No more votes 💁‍♀️", "You can't!" ]
+    end
     redirect_to colors_path
   end
 
