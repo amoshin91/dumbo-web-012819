285a905d26bdba4219389b730bc3aeb4732c964e
kevinmcalear <kevin.mcalear@gmail.com>
2019-03-13 11:35:57 -0400

    adds lecture

--- 23-intro-to-fetch/index.js
+++ 23-intro-to-fetch/index.js
@@ -1,41 +1,33 @@
-const compliments = [
-  "Your eyebrows are on fleek.",
-  "You’re a great person to do a group project with.",
-  "You’re always on time to things, which is awesome because I’m not.",
-  "Looking like a complete idiot with you is really fun.",
-  "Being alone is kind of nice. Being alone with you is better.",
-  "I would hang out with you even if you hadn’t showered for a couple days.",
-  "I would go on an 18 hour road trip with you, I like you that much.",
-  "Out of all the strangers I meet on a daily basis, I’m glad you’re one of those strangers that became my friend.",
-  "You’re that “Nothing” when people ask me what I’m thinking about.",
-  "You’re more fun than a ball pit filled with candy. (And seriously, what could be more fun than that?)",
-  "I like your outfit so much that if I wore it on the same day as you I wouldn’t even be embarrassed, we’d just be that stylish.",
-  "You’re even better than a unicorn, because you’re real.",
-  "I would be really okay with you seeing me without makeup, that’s how much I like you.",
-  "You have really good taste in friends (i.e. me).",
-  "You have a really contagious laugh. See, now I’m laughing and it's all your fault."
-];
+
 // helper method to get random number for photos
 function randomNumber(minNumber, maxNumber) {
   // given a min and max, return a number between them
   return Math.floor((Math.random() * (maxNumber - minNumber) + minNumber) / 10) * 10;
 }
 
+const createComplimentHTML = (compliment) => {
+  return `<li>
+    <div class="trash">🗑</div>
+    <div class="favorite">⭐️</div>
+    <img width="200" src="https://www.placecage.com/gif/${randomNumber(400, 500)}/${randomNumber(200, 250)}" />
+    <h1>"${compliment.message}"</h1>
+    <div>
+      <cite data-hug-count="${compliment.hug_count}">Hugged ${compliment.hug_count} times</cite>
+      <button>🤗 it</button>
+    </div>
+  </li>`;
+}
+
 const ulTag = document.querySelector('ul');
 
-compliments.forEach(function(compliment) {
-  ulTag.innerHTML +=
-  `<li>
-      <div class="trash">🗑</div>
-      <div class="favorite">⭐️</div>
-      <img width="200" src="https://www.placecage.com/gif/${randomNumber(400, 500)}/${randomNumber(200, 250)}" />
-      <h3>"${compliment}"</h3>
-      <div>
-        <cite data-hug-count="0">Hugged 0 times</cite>
-        <button>🤗 it</button>
-      </div>
-    </li>`
-})
+fetch('http://localhost:3000/compliments')
+  .then(res => res.json())
+  .then((compliments) => {
+    compliments.forEach(compliment => {
+      ulTag.innerHTML += createComplimentHTML(compliment)
+    })
+  })
+
 // be able to add a "hug" to a compliment and see my hug count go up accordingly
 
 // we need to find all of our buttons
@@ -68,15 +60,19 @@ formTag.addEventListener('submit', function(event) {
   const compliment = event.target.compliment.value
   // using that compliment make a new compliment li, add in our compliment
   // attach that new li to our UL (in the top position)
-  ulTag.innerHTML =
-  `<li>
-      <div class="trash">🗑</div>
-      <div class="favorite">⭐️</div>
-      <img width="200" src="https://www.placecage.com/gif/${randomNumber(400, 500)}/${randomNumber(200, 250)}" />
-      <h3>"${compliment}"</h3>
-      <div>
-        <cite data-hug-count="0">Hugged 0 times</cite>
-        <button>🤗 it</button>
-      </div>
-    </li>` + ulTag.innerHTML
+  ulTag.innerHTML = createComplimentHTML(compliment) + ulTag.innerHTML
 })
+
+// old way to define a function
+// function createComplimentHTML(compliment) {
+//   return `<li>
+//       <div class="trash">🗑</div>
+//       <div class="favorite">⭐️</div>
+//       <img width="200" src="https://www.placecage.com/gif/${randomNumber(400, 500)}/${randomNumber(200, 250)}" />
+//       <h1>"${compliment}"</h1>
+//       <div>
+//         <cite data-hug-count="0">Hugged 0 times</cite>
+//         <button>🤗 it</button>
+//       </div>
+//     </li>`
+// }
