a35b5e5edc5ce53ceeb0c7026bb6b0812cd3e2ee
kevinmcalear <kevin.mcalear@gmail.com>
2019-03-21 16:41:38 -0400

    adds lectures

---
+++ 26-this/index.js
@@ -0,0 +1,68 @@
+// console.log("WE GOOD")
+// the keywork "this"
+// Execution context
+
+// console.log(this)
+
+// window.console.log('test')
+
+// Scenario 1: 'this' gets AUTOMATICALLY bound to the class/instance
+// class Dog {
+//   constructor() {
+//     // console.log("what 'this' is in the dog object", this)
+//   }
+//   static whatIsADog() {
+//     // console.log("what 'this' is in the dog class method", this)
+//     return 'a dog is a loving four legged animal';
+//   }
+// }
+
+// Scenario 2: Functions
+
+// function saySup() {
+//   console.log("what 'this' is in a normal function", this)
+//   return `sup ${this.name}`
+// }
+// saySup() => this? WINDOW
+//
+// let cat = {
+//   name: "Cuddles",
+//   speak: saySup
+// }
+// cat.speak() => this? CAT OBJ
+
+
+// Scenario 3: Arrow Functions
+// class Donkey {
+//   saySup = () => {
+//     console.log("what 'this' is in an arrow function", this)
+//     return `sup`
+//   }
+// }
+//
+
+// const saySup = () => {
+//   console.log("what 'this' is in an arrow function", this)
+//   return `sup`
+// }
+// saySup() => this? WINDOW
+//
+// let cat = {
+//   name: "Cuddles",
+//   speak: saySup
+// }
+// cat.speak() => this? WINDOW
+
+
+// Scenario 4: bind/call/apply
+function saySup(name) {
+  console.log("what 'this' is in a normal function", this)
+  return `sup ${name}`
+}
+
+let cat = {
+  name: "Cuddles",
+  speak: saySup
+}
+
+let ourBoundSaySup = saySup.bind(cat);
